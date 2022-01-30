a0617b7bf88a0e61dcab9e0e476da03df4f6c1ab
Dominic Ritzmann <dominic.ritzmann@noseryoung.ch>
2022-01-30 21:30:02 +0100

    added test

---
+++ src/com/company/DeliverOrderTest.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeliverOrderTest {
+
+    @Test
+    void makeBurger() {
+    }
+
+    @Test
+    void chooseOrder() {
+    }
+
+    @Test
+    void deliverOrderedBurger() {
+    }
+}
--- src/com/company/OrderTest.java
+++ src/com/company/OrderTest.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
 
-    @org.junit.jupiter.api.Test
+    Menu menu = new Menu();
+
+    @Test
     void createBurger() {
+        Order.CreateBurger(menu);
+
+        if (menu.getBurgerList().size() == 1) {
+            assertTrue(true);
+        }
     }
 }
